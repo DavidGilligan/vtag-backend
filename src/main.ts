739ea4a1b3efa62ad377b22f289ba221bb273afc
DavidGilligan <David.gilligan1997@gmail.com>
2026-06-13 20:19:02 +0100

    Allow GitHub Pages frontend through CORS

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+app.enableCors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://davidgilligan.github.io',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+});
 
   app.useStaticAssets(join(process.cwd(), 'uploads'), {
     prefix: '/uploads/',
